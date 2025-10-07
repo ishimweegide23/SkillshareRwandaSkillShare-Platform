5806af39a0ed1d7dbc7b30a623c85ab0c408c68d
kwibuka <ishimweegide20@gmail.com>
2025-10-08 00:59:43 +0300

    Update SkillshareApplicationTests.java

--- backend/src/test/java/com/skillshare/SkillshareApplicationTests.java
+++ backend/src/test/java/com/skillshare/SkillshareApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest  kdkd
+@SpringBootTest  
 @ActiveProfiles("test")
 @TestPropertySource(locations = "classpath:application-test.properties")
 class SkillshareApplicationTests {
